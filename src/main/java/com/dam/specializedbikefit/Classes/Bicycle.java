b640adb3f32ad1b95ce7a5ed88d580439a971c16
TuroRV <turo@hotmail.es>
2025-11-21 13:54:14 +0100

    método login comprueba las credenciales del usuario. falta añadir la carga del nuevo stage.

--- src/main/java/com/dam/specializedbikefit/Classes/Bicycle.java
+++ src/main/java/com/dam/specializedbikefit/Classes/Bicycle.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Set;
 
 @Entity
+@Table(name = "bicycles")
 public class Bicycle {
 
     @Id
--- src/main/java/com/dam/specializedbikefit/Controllers/LoginController.java
+++ src/main/java/com/dam/specializedbikefit/Controllers/LoginController.java
@@ -1,9 +1,14 @@
 package com.dam.specializedbikefit.Controllers;
 
+import com.dam.specializedbikefit.Classes.User;
+import com.dam.specializedbikefit.DBConnection.Connection;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 public class LoginController {
 
@@ -18,6 +23,23 @@ public class LoginController {
     protected void login() {
         String user_email = emailTextField.getText();
         String password = passwordTextField.getText();
+
+        Session session = Connection.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+
+        Query<User> query = session.createQuery("FROM User WHERE user_email = :email AND user_password = :password",User.class);
+        query.setParameter("email", user_email);
+        query.setParameter("password", password);
+
+
+        //Esto devuelve un único valor de usuario. Si no es nulo, significa que las credenciales con correctas.
+        User user = query.uniqueResult();
+
+        if (user != null) {
+            //Falta
+        }
+
+
 
 
     }
