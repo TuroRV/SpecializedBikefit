759dd650fbe234af75dd1ba1f49f1248fd0b4869
TuroRV <turo@hotmail.es>
2025-12-07 23:43:59 +0100

    completar diseño del garage-view. formulario cumpliendo requisitos

--- src/main/java/com/dam/specializedbikefit/Controllers/GarageController.java
+++ src/main/java/com/dam/specializedbikefit/Controllers/GarageController.java
@@ -1,7 +1,7 @@
 package com.dam.specializedbikefit.Controllers;
 
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
 
 public class GarageController {
 
@@ -13,10 +13,40 @@ public class GarageController {
     public TableColumn reachColumn;
     public TableColumn seatTubeColumn;
     public TableColumn topTubeColumn;
+    public TableColumn bikeIsEbikeColumn;
+    public TextField brandField;
+    public TextField modelField;
+    public TextField yearField;
+    public RadioButton radioS1;
+    public ToggleGroup sizeGroup;
+    public RadioButton radioS2;
+    public RadioButton radioS3;
+    public RadioButton radioS4;
+    public RadioButton radioS5;
+    public CheckBox isEbikeCheck;
+    public Label reachValueLabel;
+    public Slider reachSlider;
+    public Label stackValueLabel;
+    public Slider stackSlider;
+    public Label seatTubeValueLabel;
+    public Slider seatTubeSlider;
+    public Label topTubeValueLabel;
+    public Slider topTubeSlider;
 
     public void initialize(){
-
+        setSliderConfig(reachSlider, reachValueLabel);
+        setSliderConfig(stackSlider, stackValueLabel);
+        setSliderConfig(seatTubeSlider, seatTubeValueLabel);
+        setSliderConfig(topTubeSlider, topTubeValueLabel);
     }
+    public void setSliderConfig(Slider slider, Label label){
+        label.setText((int)slider.getValue() + " mm");
+        slider.valueProperty().addListener((observable, oldValue, newValue) -> {
+            label.setText((newValue.intValue() + " mm"));
+        });
+        }
 
 
+    public void addBicycle(ActionEvent actionEvent) {
+    }
 }
